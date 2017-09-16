f38f0b5294332da11ce35b2b1ed486d75491a0dd
Arnav Borborah <arnavborborah11@gmail.com>
2017-09-16 15:32:06 +0000

    Changed grouping

--- solutions/problem-one/problem-one.cpp
+++ solutions/problem-one/problem-one.cpp
@@ -24,7 +24,7 @@ int main()
     for (int i = 0; i < 1000; ++i)
     {
         // Add i to sum if it is divisible by 3 or 5
-        sum += ((i % 5) == 0 || (i % 3) == 0) ? 
+        sum += ((i % 5 == 0) || (i % 3 == 0)) ? 
             i : 0;
     }
     std::cout << "The sum of the multiples of 3 and 5 under 1000 is " << sum << ".";
