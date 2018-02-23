e56951f2629b9314be782710217ea4eefc3895b1
Arnav Borborah <arnavborborah11@gmail.com>
2018-02-23 12:51:16 +0000

    Added digit separators

--- solutions/Problem037.cpp
+++ solutions/Problem037.cpp
@@ -75,7 +75,7 @@ bool isTruncPrime(long long int number, const std::vector<long long int>& primeL
 
 int main()
 {
-    const std::vector<long long int> primesUptoMillion = primesUpto(1000000); // Represents all the primes up to 1 million
+    const std::vector<long long int> primesUptoMillion = primesUpto(1'000'000LL); // Represents all the primes up to 1 million
     int numberTruncatablePrimes = 0;
     long long int currentNumber = 9; // 2, 3, 5, and 7 are not included in the search for truncatable primes
     long long int truncatablePrimeSum = 0;
