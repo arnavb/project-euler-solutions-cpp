75f7c8297d9f566f9b488fb84f2f77d0725c2cfe
Arnav Borborah <arnavborborah11@gmail.com>
2018-03-02 23:36:52 +0000

    Added colored output

--- test/test_all_solutions.py
+++ test/test_all_solutions.py
@@ -6,11 +6,21 @@ import sys # For command line arguments
 import distutils # For copying the data directory
 from distutils import dir_util # https://stackoverflow.com/a/33417555/6525260
 
+class colors: # https://stackoverflow.com/a/287944/6525260
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+
 def test_all_solutions(directory_name, compiler_name, expected_solutions):
     files = glob.glob(directory_name + "/Problem***.cpp")
     
     if not files:
-        print("Error: No files were found!")
+        print(colors.FAIL + "Error: No files were found!" + colors.ENDC)
         return 1
         
     files.sort()
@@ -20,8 +30,8 @@ def test_all_solutions(directory_name, compiler_name, expected_solutions):
     try:
         dir_util.copy_tree(directory_name + "/data/", "./data/") # Copy the data folder that some solutions need
     except dir_util.DistutilsFileError:
-        print("The data folder was unable to be copied from " + directory_name + "/data/ to ./data/!")
-        print("Some solutions may not be able to compile!")
+        print(colors.WARNING + "The data folder was unable to be copied from " + directory_name + "/data/ to ./data/!")
+        print("Some solutions may not be able to compile!\n" + colors.ENDC)
 
     passed_tests = 0
     
@@ -29,12 +39,12 @@ def test_all_solutions(directory_name, compiler_name, expected_solutions):
         compile_command = compiler_name + " " + file + " -std=c++14 -O2"
         current_key = file[-7: -4]
         
-        print("===============")
-        print("TEST: Problem " + str(current_key) + "\n")
+        print(colors.BOLD + "===============" + colors.ENDC)
+        print(colors.OKBLUE + "TEST: " + colors.ENDC + "Problem " + str(current_key) + "\n")
         print("Trying to compile " + file + " with " + compile_command)
         
         if (subprocess.call(compile_command.split(), stdout = subprocess.DEVNULL, stderr = subprocess.STDOUT) != 0): # Compile the current file
-            print("FAIL: File was unable to be compiled!")
+            print("Result: " + colors.FAIL + "FAILURE: File was unable to be compiled!" + colors.ENDC)
         else:
             try:
                 print("File was successfully able to be compiled. Running executable with ./a.out\n")
@@ -48,16 +58,16 @@ def test_all_solutions(directory_name, compiler_name, expected_solutions):
                     print("Expected output: " + str(expected_solution))
                     
                     if current_output == expected_solution:
-                        print("\nResult: SUCCESS")
+                        print("\nResult: " + colors.OKGREEN +  "SUCCESS" + colors.ENDC)
                         passed_tests += 1
                     else:
-                        print("\nResult: FAIL: Current output " + str(current_output) + " does not match expected output " + str(expected_solution) + "!")
+                        print("\nResult: " + colors.FAIL + "FAILURE: Current output " + str(current_output) + " does not match expected output " + str(expected_solution) + "!" + colors.ENDC)
                 else:
-                    print("FAIL: Expected solution to problem " + str(current_key) + " was not found!")
+                    print("Result: " + colors.FAIL + "FAILURE: Expected solution to problem " + str(current_key) + " was not found!" + colors.ENDC)
             except ValueError:
-                print("FAIL: Output is of non-integer type!")
+                print("Result: " + colors.FAIL + "FAILURE: Output is of non-integer type!" + colors.ENDC)
             
-    print("===============")        
+    print(colors.BOLD + "===============" + colors.ENDC)        
         
     if os.path.isfile('./a.out'): # Remove the new executable file
         os.remove('a.out')
@@ -68,11 +78,13 @@ def test_all_solutions(directory_name, compiler_name, expected_solutions):
     
     num_tests = len(expected_solutions)
     
-    print(str(passed_tests) + " out of " + str(num_tests) + " tests were successful.")
+    print(colors.BOLD + str(passed_tests) + " out of " + str(num_tests) + " tests were successful." + colors.ENDC)
     
     if passed_tests == num_tests:
+        print("Result: " + colors.OKGREEN +  "SUCCESS" + colors.ENDC)
         return 0
     else:
+        print("Result: " + colors.FAIL + "FAILURE" + colors.ENDC)
         return 1
     
     
@@ -96,7 +108,7 @@ if __name__ == '__main__':
     
     if len(sys.argv) != 3: 
         print("Error: Missing operands!")
-        print("Usage: " + sys.argv[0] + " path/to/problems compile-command")
+        print("Usage: " + sys.argv[0] + " path/to/problems compiler-name")
         sys.exit(1)
     elif not os.path.exists(sys.argv[1]):
         print("Error: Path " + sys.argv[1] + " does not exist!")
