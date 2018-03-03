a49eedd96b1605f99afb341d1ba2eccd9cff3ce4
Arnav Borborah <arnavborborah11@gmail.com>
2018-03-03 17:54:21 +0000

    Shortened build script

--- tests/test_all_solutions.py
+++ tests/test_all_solutions.py
@@ -6,33 +6,20 @@ import sys # For command line arguments
 import distutils # For copying the data directory
 from distutils import dir_util # https://stackoverflow.com/a/33417555/6525260
 
-class colors:
-    OKBLUE = '\033[36;1m'
-    OKGREEN = '\033[32;1m'
-    WARNING = '\033[33;1m'
-    FAIL = '\033[31;1m'
-    ENDC = '\033[0m'
-    BOLD = '\033[1m'
-    UNDERLINE = '\033[4m'
+class colors: # ANSI escape codes for colors
+    OKBLUE = "\033[36;1m"
+    OKGREEN = "\033[32;1m"
+    WARNING = "\033[33;1m"
+    FAIL = "\033[31;1m"
+    ENDC = "\033[0m"
+    BOLD = "\033[1m"
+    UNDERLINE = "\033[4m"
+    BRIGHTMAGENTA = "\033[95;1m"
 
 def color_string(color, string):
     return color + string + colors.ENDC
 
 def test_all_solutions(directory_name, compiler_name, expected_solutions):
-    files = glob.glob(directory_name + "/Problem***.cpp")
-    
-    if not files:
-        print(color_string(colors.FAIL, "Error: No files were found!"))
-        return 1
-        
-    files.sort()
-    
-    print("\nThe following files were found: ")
-    
-    print(*files, sep = "\n")
-    
-    print("\n")
-    
     try:
         dir_util.copy_tree(directory_name + "/data/", "./data/") # Copy the data folder that some solutions need
     except dir_util.DistutilsFileError:
@@ -40,47 +27,46 @@ def test_all_solutions(directory_name, compiler_name, expected_solutions):
 
     passed_tests = 0
     
-    for file in files:
-        compile_command = compiler_name + " " + file + " -std=c++14 -O2"
-        current_key = file[-7: -4]
-        
+    for problem, solution in expected_solutions.items():
         print(color_string(colors.BOLD, "==============="))
-        print(color_string(colors.OKBLUE, "TEST") + ": Problem " + str(current_key) + "\n")
-        print("Trying to compile " + file + " with " + compile_command)
+        print(color_string(colors.OKBLUE, "TEST") + ": Problem " + str(problem))
+        
+        current_file = directory_name + "/Problem" + problem + ".cpp"
+        
+        print("\nFile: " + current_file)
+        
+        compile_command = compiler_name + " " + current_file + " -std=c++14 -O2"
+        
+        print("Running: " + compile_command)
         
         if (subprocess.call(compile_command.split(), stdout = subprocess.DEVNULL, stderr = subprocess.STDOUT) != 0): # Compile the current file
             print("Result: " + color_string(colors.FAIL, "FAILURE") + ": File was unable to be compiled!")
         else:
-            try:
-                print("File was successfully able to be compiled. Running executable with ./a.out\n")
-                current_output = int(subprocess.check_output(['./a.out']).decode('utf-8')) # Execute the file and capture output
+            current_output_string = subprocess.check_output(['./a.out']).decode('utf-8') # Run the executable and capture the output
+            if current_output_string.strip().isdigit():
+                current_output = int(current_output_string)
                 
-                print("Current output: " + str(current_output))
+                print("\nExpected output: " + color_string(colors.BRIGHTMAGENTA, str(solution)))
+                print("Current output: " + color_string(colors.BRIGHTMAGENTA, str(current_output)))
                 
-                if current_key in expected_solutions:
-                    expected_solution = expected_solutions[current_key] # Extract problem number for indexing solution
-                    
-                    print("Expected output: " + str(expected_solution))
-                    
-                    if current_output == expected_solution:
-                        print("\nResult: " + color_string(colors.OKGREEN, "SUCCESS"))
-                        passed_tests += 1
-                    else:
-                        print("\nResult: " + color_string(colors.FAIL, "FAILURE") + ": Current output " + str(current_output) + " does not match expected output " + str(expected_solution) + "!")
+                print("\nResult: ", end = '')
+                if current_output == solution:
+                    print(color_string(colors.OKGREEN, "SUCCESS"))
+                    passed_tests += 1
                 else:
-                    print("Result: " + color_string(colors.FAIL, "FAILURE") + ": Expected solution to problem " + str(current_key) + " was not found!")
-            except ValueError:
-                print("Result: " + color_string(colors.FAIL, "FAILURE") + ": Output is of non-integer type!")
-            
-    print(color_string(colors.BOLD, "==============="))        
+                    print(color_string(colors.FAIL, "FAILURE") + ": Current output " + str(current_output) + " does not match expected output " + str(solution) + "!")
+            else:
+                print("Result: " + color_string(colors.FAIL, "FAILURE") + ": Expected integer but found: ")
+                print(current_output_string)
+    
+    print(color_string(colors.BOLD, "==============="))
+        
         
     if os.path.isfile('./a.out'): # Remove the new executable file
         os.remove('a.out')
     
     if os.path.isdir("./data"): # Remove the data directory
         shutil.rmtree("./data")
-    
-    print("\n")
     
     num_tests = len(expected_solutions)
     
