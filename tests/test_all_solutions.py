4ceee36de26608d4640306a68683fe7d6f71bc54
Arnav Borborah <arnavborborah11@gmail.com>
2018-03-03 20:24:52 +0000

    Literal string interpolation

--- tests/test_all_solutions.py
+++ tests/test_all_solutions.py
@@ -23,40 +23,40 @@ def test_all_solutions(directory_name, compiler_name, expected_solutions):
     try:
         dir_util.copy_tree(directory_name + "/data/", "./data/") # Copy the data folder that some solutions need
     except dir_util.DistutilsFileError:
-        print(color_string(colors.WARNING, "The data folder was unable to be copied from " + directory_name + "/solutions to ./data!\nSome solutions may not be able to compile!\n"))
+        print(color_string(colors.WARNING, f"The data folder was unable to be copied from {directory_name} to ./data!\nSome solutions may not be able to compile!\n"))
 
     passed_tests = 0
     
     for problem, solution in expected_solutions.items():
         print(color_string(colors.BOLD, "==============="))
-        print(color_string(colors.OKBLUE, "TEST") + ": Problem " + str(problem))
+        print(f"{color_string(colors.OKBLUE, 'TEST')}: Problem {str(problem)}")
         
         current_file = directory_name + "/Problem" + problem + ".cpp"
         
         print("\nFile: " + current_file)
         
-        compile_command = compiler_name + " " + current_file + " -std=c++14 -O2"
+        compile_command = f"{compiler_name} {current_file} -std=c++14 -O2"
         
         print("Running: " + compile_command)
         
         if (subprocess.call(compile_command.split(), stdout = subprocess.DEVNULL, stderr = subprocess.STDOUT) != 0): # Compile the current file
-            print("Result: " + color_string(colors.FAIL, "FAILURE") + ": File was unable to be compiled!")
+            print(f"Result: {color_string(colors.FAIL, 'FAILURE')}: File was unable to be compiled!")
         else:
             current_output_string = subprocess.check_output(['./a.out']).decode('utf-8') # Run the executable and capture the output
             if current_output_string.strip().isdigit():
                 current_output = int(current_output_string)
                 
-                print("\nExpected output: " + color_string(colors.BRIGHTMAGENTA, str(solution)))
-                print("Current output: " + color_string(colors.BRIGHTMAGENTA, str(current_output)))
+                print(f"\nExpected output: {color_string(colors.BRIGHTMAGENTA, str(solution))}")
+                print(f"Current output: {color_string(colors.BRIGHTMAGENTA, str(current_output))}")
                 
                 print("\nResult: ", end = '')
                 if current_output == solution:
                     print(color_string(colors.OKGREEN, "SUCCESS"))
                     passed_tests += 1
                 else:
-                    print(color_string(colors.FAIL, "FAILURE") + ": Current output " + str(current_output) + " does not match expected output " + str(solution) + "!")
+                    print(f"{color_string(colors.FAIL, 'FAILURE')}: Current output {str(current_output)} does not match expected output {str(solution)}!")
             else:
-                print("Result: " + color_string(colors.FAIL, "FAILURE") + ": Expected integer but found: ")
+                print(f"Result: {color_string(colors.FAIL, 'FAILURE')}: Expected integer but found: ")
                 print(current_output_string)
     
     print(color_string(colors.BOLD, "==============="))
@@ -74,10 +74,10 @@ def test_all_solutions(directory_name, compiler_name, expected_solutions):
     print(color_string(colors.BOLD, str(passed_tests) + " out of " + str(num_tests) + " tests were successful."))
     
     if passed_tests == num_tests:
-        print("Result: " + color_string(colors.OKGREEN, "SUCCESS"))
+        print(f"Result: {color_string(colors.OKGREEN, 'SUCCESS')}")
         return 0
     else:
-        print("Result: " + color_string(colors.FAIL, "SUCCESS"))
+        print(f"Result: {color_string(colors.FAIL, 'SUCCESS')}")
         return 1
     
     
@@ -96,9 +96,9 @@ if __name__ == '__main__':
     
     if len(sys.argv) != 3: 
         print("Error: Missing operands!")
-        print("Usage: " + sys.argv[0] + " repository-root compiler-name")
+        print(f"Usage: {sys.argv[0]} repository-root compiler-name")
         sys.exit(1)
     elif not os.path.exists(sys.argv[1]):
-        print("Error: Path " + sys.argv[1] + " does not exist!")
+        print(f"Error: Path {sys.argv[1]} does not exist!")
     else:
         sys.exit(test_all_solutions(sys.argv[1] + "/solutions", sys.argv[2], expected_solutions))
