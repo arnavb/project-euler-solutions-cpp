85e6198c6d8f93163a49ec996695ef83f5453c70
Arnav Borborah <arnavborborah11@gmail.com>
2018-03-28 15:09:44 +0000

    Added solution to problem #009

---
+++ solutions/Problem009.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+int main()
+{
+    for (int a = 1; a < 1000; ++a)
+    {
+        for (int b = 1; b < 1000; ++b)
+        {
+            for (int c = 1; c < 1000; ++c)
+            {
+                if (a * a + b * b == c * c && a + b + c == 1000)
+                {
+                    std::cout << a * b * c << "\n";
+                    return 0;
+                }
+            }
+        }
+    }
+}
